8bd4fd024b609bd50be4974ca76f0dc63c0d44e8
Sabeer Ali <sabeer-ali@users.noreply.github.com>
2020-06-06 21:51:29 +0530

    Initial Stage 01
    Scen Hrms - Testing PWA

---
+++ precache-manifest.29e5a501407d2ba52973a58234c00fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3e66cd1ca4648d4c1c2fbff529d6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6024132eb714154e52df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e14cc0dcdbf1342e24b8",
+    "url": "/static/js/2.64c9eae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64c9eae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6024132eb714154e52df",
+    "url": "/static/js/main.a5ed165b.chunk.js"
+  },
+  {
+    "revision": "e1ce4f175cf04914eb0a",
+    "url": "/static/js/runtime-main.ef738670.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
